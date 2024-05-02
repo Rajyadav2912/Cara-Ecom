ff2989cc863388b75fca0c8f383365ca86a2fd16
Raj Yadav <rajyadav7047@gmail.com>
2024-05-02 23:20:54 +0530

    Commit 2

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = ({ brand, title, rating, price, description, image }) => {
+  return <div>
+    <div>
+        <img src="" alt="" />
+    </div>
+    <div>
+        
+    </div>
+  </div>;
+};
+
+export default Card;
--- src/components/FeatureProduct/FeatureProduct.jsx
+++ src/components/FeatureProduct/FeatureProduct.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import "../FeatureProduct/FeatureProduct.css";
+import data from "../../data";
+import Card from "../Card/Card";
 
 const FeatureProduct = () => {
+  const product = data.product.men;
+
   return (
     <div className="product">
       <h2 className="mx-auto font-semibold"> Feature Product</h2>
       <p>Summer Collection New Morden Design</p>
+      {product.map((value, index) => {
+        return (
+          <Card
+            key={index}
+            brand={value.brand}
+            title={value.title}
+            rating={value.rating}
+            price={value.price}
+            description={value.description}
+            image={value.image}
+          />
+        );
+      })}
     </div>
   );
 };
